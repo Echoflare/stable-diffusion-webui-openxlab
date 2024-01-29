743e6c65f8a2f8327de3d69025992ee6d93f9e36
Echoflare <144500846+Echoflare@users.noreply.github.com>
2024-01-29 15:02:46 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import os
 
 os.chdir(f"/home/xlab-app-center")
-os.system(f"git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui")
+os.system(f"git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui /home/xlab-app-center/stable-diffusion-webui")
 os.chdir(f"/home/xlab-app-center/stable-diffusion-webui")
 os.system("python launch.py")
